a45604844b49afbfc5e1f26ecd933e340c1272c6
abramovvicz <abramovvicz@gmail.com>
2019-02-07 17:53:09 +0100

    commit message

--- src/main/java/DAO/IPresenceList.java
+++ src/main/java/DAO/IPresenceList.java
@@ -1,4 +1,22 @@
 package DAO;
 
+import Model.TrainingDay;
+import Model.User;
+
+import java.util.List;
+
 public interface IPresenceList {
+
+
+    List<User> checkPresenceByUser(String name);
+
+    List<User> checkPresenceByUser(User user);
+
+    List<User> checkPresenceByTrainingDay(TrainingDay trainingDay);
+
+    boolean checkPresenceByAll();
+
+    boolean addPresence(User user, TrainingDay trainingDay);
+
+    boolean editPresence(User user, TrainingDay trainingDay);
 }
--- src/main/java/DAO/PresenceListMem.java
+++ src/main/java/DAO/PresenceListMem.java
@@ -1,4 +1,52 @@
 package DAO;
 
-public class PresenceListMem {
+import Model.TrainingDay;
+import Model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PresenceListMem implements IPresenceList {
+
+    private List<User> presenceUserList = new ArrayList<>();
+    private IUser user = new UserMem();
+
+//jebać
+    public PresenceListMem(IUser user) {
+        this.user = user;
+    }
+
+
+    @Override
+    public List<User> checkPresenceByUser(String name) {
+//        presenceUserList.stream().filter(x->x.getUser(user.getUserByName(name))).findAny().orElse(null);
+//        presenceUserList.stream().findAny().filter(x->x.getUser(user.getUserByName(name)))
+        return null;
+    }
+
+    @Override
+    public List<User> checkPresenceByUser(User user) {
+//
+        return null;
+    }
+
+    @Override
+    public List<User> checkPresenceByTrainingDay(TrainingDay trainingDay) {
+        return null;
+    }
+
+    @Override
+    public boolean checkPresenceByAll() {
+        return false;
+    }
+
+    @Override
+    public boolean addPresence(User user, TrainingDay trainingDay) {
+        return false;
+    }
+
+    @Override
+    public boolean editPresence(User user, TrainingDay trainingDay) {
+        return false;
+    }
 }
