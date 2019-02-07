bcf791b763a7ba891cacde5d8ff03fcd99cd8d01
abramovvicz <abramovvicz@gmail.com>
2019-02-07 15:06:06 +0100

    add interface IUser

--- src/main/java/Model/User.java
+++ src/main/java/Model/User.java
@@ -5,4 +5,43 @@ public class User {
     private String name;
     private String mail;
     private String number;
+
+    public User(int id, String name, String mail, String number) {
+        this.id = id;
+        this.name = name;
+        this.mail = mail;
+        this.number = number;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
 }
